885673821fa5ff3cb77f87a53cba5f9b049f0337
Ginzex <aginzex@gmail.com>
2025-05-10 14:24:59 +0300

    added input file

---
+++ python/day1_part1.py
@@ -0,0 +1,11 @@
+from resources import *
+
+def main():
+    input_path = "resources\input.txt"
+    with open(input_path) as f :
+        for line in f:
+            
+    lst = [x for x in df if 2020 - x ==0]
+    print[lst]
+if __name__ == "__main__":
+    main()
