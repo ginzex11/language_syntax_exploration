626e99a47b3d0d47ee049e84c1eb82e1716993bb
Ginzex <aginzex@gmail.com>
2025-08-24 15:45:29 +0300

    base chekpoint for day1 in advent 2024

--- python/day1_part1.py
+++ python/day1_part1.py
@@ -1,11 +1,19 @@
 from resources import *
 
-def main():
-    input_path = "resources\input.txt"
-    with open(input_path) as f :
-        for line in f:
-            
-    lst = [x for x in df if 2020 - x ==0]
-    print[lst]
+def parse_file(file_path):
+    list_1=[]
+    list_2 = []
+    
+    with open(file_path,'r') as file:
+        for line in file:
+            num_1,num_2 = map(int,line.strip().split())
+            list_1.append(num_1)
+            list_2.append(num_2)
+        return list_1,list_2
+    
+
+
 if __name__ == "__main__":
-    main()
+    file_path = "resources\input_1_p_1"
+    column_1, column_2 =parse_file(file_path)
+    print("hi")
